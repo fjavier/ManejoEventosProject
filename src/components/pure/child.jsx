665c0f8ec5b07e043cf1c9cce2ea8e6610102d74
Francisco Javier Briceño <fjavier.cgam@gmail.com>
2022-09-07 18:10:57 +0000

    Se agrego input con eventos onFocus y onChange

--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -15,10 +15,14 @@ const Child = () => {
 
   return (
     <div>
-      <h1 onMouseOver={()=>console.log("Mouse Over")}>Child</h1>
+      <h1 onMouseOver={() => console.log("Mouse Over")}>Child</h1>
       <button onClick={() => console.log("Button 1 Pulsado")}>Boton 1</button>
       <button onClick={pressButton}>Boton 2</button>
-      <button onClick={()=>pressButtonParam("texto a mostrar")}>Boton 3</button>
+      <button onClick={() => pressButtonParam("texto a mostrar")}>Boton 3</button>
+      <input 
+        onFocus={()=>console.log("Se invoco al focus")} 
+        onChange={(event)=>console.log(event.target.value)}
+        placeholder="Inserte texto"></input>
     </div>
   );
 }
