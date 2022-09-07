ea997a97dbfb0380cc57cb8e62515e3523022e2f
Francisco Javier Briceño <fjavier.cgam@gmail.com>
2022-09-07 18:45:30 +0000

    Se crea formulario para manejo de eventos compartidos de componentes padres con componentes hijos.

--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {useRef} from "react";
 
-const Child = () => {
-  function pulsarButton() {
+const Child = ({ sendAction, name, updateNameAction }) => {
 
-  }
-
+  const messageInput = useRef("");
+  const nameInput = useRef("");
+  
   function pressButton() {
     console.log("Alerta por defecto");
   }
@@ -13,16 +13,37 @@ const Child = () => {
     console.log("Boton presionado :" + param);
   }
 
+  function showMessage() {
+    const message = messageInput.current.value;
+    console.log("Message: "+message);
+  }
+
+  function sendNewName(event){
+    event.preventDefault();
+    updateNameAction(nameInput.current.value);
+  }
+
   return (
     <div>
-      <h1 onMouseOver={() => console.log("Mouse Over")}>Child</h1>
+      <h1 onMouseOver={() => console.log("Mouse Over")}>Hola {name}</h1>
       <button onClick={() => console.log("Button 1 Pulsado")}>Boton 1</button>
       <button onClick={pressButton}>Boton 2</button>
-      <button onClick={() => pressButtonParam("texto a mostrar")}>Boton 3</button>
-      <input 
-        onFocus={()=>console.log("Se invoco al focus")} 
-        onChange={(event)=>console.log(event.target.value)}
+      <button onClick={showMessage}>Boton 3</button>
+
+      <input
+        ref={messageInput}
+        onFocus={() => console.log("Se invoco al focus")}
+        onChange={(event) => console.log(event.target.value)}
         placeholder="Inserte texto"></input>
+      <button
+        onClick={()=>sendAction(messageInput.current.value)}>SendMessage from child to Father</button>
+
+      <div style={{marginTop:"10px"}}>
+        <form onSubmit={sendNewName}>
+          <input ref={nameInput} placeholder="Name"></input>
+          <button type="submit">Update name</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/pure/father.jsx
+++ src/components/pure/father.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import Child from "./child"
 
-const Father = () =>{
+const Father = () => {
+  const [name, setName] = useState("Javier");
+  
+  const sendMessage = (message) => {
+    console.log("Mensaje recibido: " + message);
+  }
+
+  const updateName = (newName)=>{
+    setName(newName);    
+  }
   
   return (
     <div>
       <h1>Father</h1>
-      <Child></Child>
+      <Child name={name} 
+        sendAction={sendMessage} 
+        updateNameAction={updateName}></Child>
     </div>
   );
 }
